f408dabe22cca16b540bad8791e5568f523e26e9
Tapuzi <anotahacou@gmail.com>
2024-06-13 01:36:24 +0300

    Dev typo, add description

--- run_action.sh
+++ run_action.sh
@@ -1,3 +1,3 @@
 export DOCKER_HOST=unix:///run/user/1000/docker.sock
 export DOCKER_BUILDKIT=1
-./bin/act -v --job unit_tests_thin -P namespace-profile-amd64-8x16=ubuntu:latest | ts
+./bin/act --job unit_tests_thin -P namespace-profile-amd64-8x16=ubuntu:latest | ts
